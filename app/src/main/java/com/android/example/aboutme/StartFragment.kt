f8b4ccfb382d1f573164630ce5888b295de16ff2
RonCalderon <rpcalderon@up.edu.ph>
2020-03-19 09:57:59 +0800

    fix:reverted everything to support fragments feat: changed startfragment kt and xml

--- app/src/main/java/com/android/example/aboutme/StartFragment.kt
+++ app/src/main/java/com/android/example/aboutme/StartFragment.kt
@@ -1,24 +1,18 @@
 package com.android.example.aboutme
 
 import android.os.Bundle
-import android.support.v4.app.Fragment
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.databinding.DataBindingUtil
 
 /**
  * A simple [Fragment] subclass.
  */
 class StartFragment : Fragment() {
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?): View? {
-            val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater,
-                R.layout.fragment_title,container,false)
-            return binding.root
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_start_screen, container, false)
     }
-
 }
