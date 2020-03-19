a6cde22356e9146502abc47ba8a6e08eeb6f5237
RonCalderon <rpcalderon@up.edu.ph>
2020-03-19 09:24:05 +0800

    fix: fixed fragment by creating new one and removing unecessary features (checkboxes)

---
+++ app/src/main/java/com/android/example/aboutme/StartFragment.kt
@@ -0,0 +1,26 @@
+package com.android.example.aboutme
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class StartFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return TextView(activity).apply {
+            setText(R.string.hello_blank_fragment)
+        }
+    }
+
+
+}
