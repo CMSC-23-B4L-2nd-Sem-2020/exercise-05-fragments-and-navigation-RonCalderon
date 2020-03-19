dfc673cf03cf7d34729b8aa30e1d89842f650128
RonCalderon <rpcalderon@up.edu.ph>
2020-03-19 09:36:18 +0800

    chore:enabled data binding feat:added binding object to startfragment

--- app/src/main/java/com/android/example/aboutme/StartFragment.kt
+++ app/src/main/java/com/android/example/aboutme/StartFragment.kt
@@ -1,6 +1,5 @@
 package com.android.example.aboutme
 
-
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
@@ -15,12 +14,10 @@ class StartFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return TextView(activity).apply {
-            setText(R.string.hello_blank_fragment)
-        }
+        savedInstanceState: Bundle?): View? {
+            val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater,
+                R.layout.fragment_title,container,false)
+            return binding.root
     }
-
 
 }
